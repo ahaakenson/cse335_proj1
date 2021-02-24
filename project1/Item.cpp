b3d223d0fbea1504c7f7bcc733e96cc7f0da0853
tolmoffn <tolmoffn@msu.edu>
2021-02-24 15:28:57 -0500

    Item base class with constructor, destructor, GetX, GetY, GetWidth, GetHeight, SetLocation, GetGame, and Draw functions

--- project1/Item.cpp
+++ project1/Item.cpp
@@ -1,2 +1,49 @@
+/**
+ * \file Item.cpp
+ *
+ * \author Nikolai Tolmoff
+ */
+
 #include "pch.h"
 #include "Item.h"
+#include "Game.h"
+
+using namespace Gdiplus;
+using namespace std;
+
+/**
+ * Constructor
+ * \param game The game this item is a part of.
+ * \param filename The name of the file we are loading from.
+ */
+CItem::CItem(CGame* game, const std::wstring& filename) : mGame(game)
+{
+    mItemImage = unique_ptr<Bitmap>(Bitmap::FromFile(filename.c_str()));
+    if (mItemImage->GetLastStatus() != Ok)
+    {
+        wstring msg(L"Failed to open ");
+        msg += filename;
+        AfxMessageBox(msg.c_str());
+    }
+}
+
+/**
+ * Destructor
+ */
+CItem::~CItem()
+{
+}
+
+/**
+ * Draw the game item
+ * \param graphics Graphics device to draw on
+ */
+void CItem::Draw(Gdiplus::Graphics* graphics)
+{
+    double wid = mItemImage->GetWidth();
+    double hit = mItemImage->GetHeight();
+
+    graphics->DrawImage(mItemImage.get(),
+        float(GetX() + wid / 2), float(GetY() - hit / 2),
+        (float)mItemImage->GetWidth(), (float)mItemImage->GetHeight());
+}
--- project1/Item.h
+++ project1/Item.h
@@ -1,5 +1,71 @@
+/**
+ * \file Item.h
+ *
+ * \author Nikolai Tolmoff
+ *
+ * Base class for all game items.
+ */
+
 #pragma once
+
+#include <string>
+#include <memory>
+
+class CGame;
+
+/**
+ * Base class for all game items.
+ */
 class CItem
 {
+public:
+	/// Default contructor (disabled)
+	CItem() = delete;
+
+	/// Copy constructor (disabled)
+	CItem(const CItem&) = delete;
+
+	virtual ~CItem();
+
+	/** The X location of the item
+	 * \returns X location in pixels */
+	double GetX() const { return mX; }
+
+	/** The Y location of the item
+	 * \returns Y location in pixels */
+	double GetY() const { return mY; }
+
+	/** The width of the item
+	 * \returns width in pixels */
+	double GetWidth() const { return mItemImage->GetWidth(); }
+
+	/** The height of the item
+	 * \returns height in pixels */
+	double GetHeight() const { return mItemImage->GetHeight(); }
+
+	/// Set the item location
+	/// \param x X location
+	/// \param y Y location
+	void SetLocation(double x, double y) { mX = x; mY = y; }
+
+	/// Get the game this item is in
+	/// \returns Game pointer
+	CGame* GetGame() { return mGame; }
+
+	virtual void Draw(Gdiplus::Graphics* graphics);
+
+protected:
+	CItem(CGame* game, const std::wstring& filename);
+
+private:
+	/// The game this item is a part of
+	CGame* mGame;
+
+	// Item location on the playing field
+	double mX = 0;			///< X location for the center of the item
+	double mY = 0;			///< Y location for the center of the item
+
+	// The image of this item
+	std::unique_ptr<Gdiplus::Bitmap> mItemImage;
 };
 
