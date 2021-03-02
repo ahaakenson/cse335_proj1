751d8dd08c52e89b3bb47277ec09eac32b2db626
norrism7 <norrism7@msu.edu>
2021-03-02 09:08:48 -0800

    CControlPanel function declerations, draw and update. added a counter for the time. Wrote on Trello about my thoughts on how to complete this task

--- project1/ControlPanel.cpp
+++ project1/ControlPanel.cpp
@@ -1,2 +1,26 @@
+/**
+ * \file ControlPanel.cpp
+ *
+ * \author Matthew Norris
+ */
+
 #include "pch.h"
 #include "ControlPanel.h"
+
+
+//function that will draw the timer
+void CControlPanel::Draw(Gdiplus::Graphics* graphics)
+{
+
+}
+
+
+
+//function that will update the timer, every second
+void CControlPanel::Update(double elapsed)
+{
+
+}
+
+//Need a drawsstring function, still unsure on how to 
+//convert double to string
